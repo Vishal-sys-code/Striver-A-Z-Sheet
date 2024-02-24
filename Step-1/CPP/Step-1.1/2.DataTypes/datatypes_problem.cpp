085f78cdf24b8fe9c20801e362c24b38a6a6608c
Vishal Pandey <bishalpandey2001@gmail.com>
2024-02-24 11:06:48 +0530

    Added the datatypes_problem.cpp

--- Step-1/CPP/Step-1.1/2.DataTypes/datatypes_problem.cpp
+++ Step-1/CPP/Step-1.1/2.DataTypes/datatypes_problem.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int datatypes(string type){
-
+    if (type == "int") return 4;
+    else if (type == "Long") return 8;
+    else if (type == "Float") return 4;
+    else if (type == "Double") return 8;
+    else if (type == "Character") return 1;
+    else return -1;
 }
 
 int main(){
---
+++ Step-1/CPP/Step-1.1/2.DataTypes/datatypes_problem.cpp~
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int datatypes(string type){
+
+}
+
+int main(){
+    string type;
+    cin>>type;
+    int ans = datatypes(type);
+    cout << ans << endl;
+    return 0;
+}
